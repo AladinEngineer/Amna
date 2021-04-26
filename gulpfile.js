c579bb01fb432261d33a0b63072dd45d631ba7a5
AladinEngineer <39554532+AladinEngineer@users.noreply.github.com>
2021-04-26 21:49:24 +0500

    committing first upload

---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+const { src, dest, series, watch } = require('gulp'),
+  sourcemaps = require('gulp-sourcemaps'),
+  plumber = require('gulp-plumber'),
+  sass = require('gulp-sass'),
+  sassLint = require('gulp-sass-lint'),
+  rename = require('gulp-rename'),
+  browserSync = require('browser-sync').create(),
+  babel = require('gulp-babel'),
+  uglify = require('gulp-uglify'),
+  nodemon = require('gulp-nodemon'),
+  del = require('del'),
+  iconfont = require('gulp-iconfont'),
+  iconfontCss = require('gulp-iconfont-css');
+
+const paths = {
+  style: {
+    src: 'src/assets/scss/**/*.scss',
+    dest: `public/css/`,
+    watchFiles: 'src/assets/scss/**/*.scss',
+  },
+  js: {
+    src: 'src/assets/js/main.js',
+    dest: 'public/js',
+    watchFiles: 'src/assets/js/**/*.js',
+  },
+  icons: {
+    src: 'public/img/svg/*.svg',
+    dest: 'public/www/svg',
+  },
+  sassLint: '.sass-lint.yml',
+};
+
+const Style = () => {
+  return src(paths.style.src)
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(
+      sassLint({
+        options: {
+          configFile: paths.sassLint,
+        },
+      })
+    )
+    .pipe(sassLint.format())
+    .pipe(sass.sync({ outputStyle: 'compressed' }))
+    .on('error', sass.logError)
+    .pipe(
+      rename({
+        basename: 'app',
+        suffix: '.min',
+      })
+    )
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest(paths.style.dest))
+    .pipe(browserSync.stream());
+};
+
+const Script = () => {
+  return src(paths.js.src)
+    .pipe(
+      babel({
+        presets: ['@babel/preset-env'],
+      })
+    )
+    .pipe(uglify())
+
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest(paths.js.dest))
+    .pipe(browserSync.stream());
+};
+
+// ICON FONT -> CONVERT SVG ICONS TO FONT SVG ICONS
+const SvgFont = () => {
+  return src(paths.icons.src)
+    .pipe(
+      iconfontCss({
+        fontName: 'svgicons',
+        cssClass: 'icon',
+        path: 'src/assets/config/icon-font.scss',
+        targetPath: '../../../src/assets/scss/fonts/_icon-font.scss',
+        fontPath: 'svg/',
+      })
+    )
+    .pipe(
+      iconfont({
+        fontName: 'svgicons',
+        prependUnicode: false,
+        formats: ['ttf', 'woff', 'eot'],
+        fontHeight: 1000,
+        normalize: true,
+        centerHorizontally: true,
+      })
+    )
+    .pipe(dest(paths.icons.dest));
+};
+
+const Clean = () => {
+  return del(['public/css', 'public/js'], { force: true });
+};
+
+const develop = () => {
+  return nodemon({
+    script: 'app.js',
+    watch: ['app.js', 'src/**/*'],
+    ignore: ['gulpfile.js', 'node_modules/**', 'public/**', 'src/assets/js'],
+  }).on('restart', () => {
+    browserSync.reload();
+  });
+};
+
+// WATCHING
+const Watching = (cb) => {
+  watch(paths.style.watchFiles, Style);
+  watch(paths.js.watchFiles, Script);
+  develop(cb);
+};
+
+const build = series(Clean, SvgFont, Style, Script, Watching);
+
+exports.default = build;
---
+++ plan.md
@@ -0,0 +1,33 @@
+<!-- :black_circle: -->
+<!-- :heavy_check_mark: -->
+
+1. Create directory structure :heavy_check_mark:
+2. Gulp.js configuration <br>
+
+   - set src and dest folder :heavy_check_mark:<br>
+   - add scss, js tasks :heavy_check_mark:<br>
+   - gulp-nodemon install :heavy_check_mark:<br>
+   - 100% automatization :heavy_check_mark:<br>
+
+3. Config static folders/files :heavy_check_mark:<br>
+4. Integrate bootstrap 5 and jquery :heavy_check_mark:<br>
+5. Add fonts <br>
+
+   - create \_fonts.scss :heavy_check_mark:<br>
+   - path config in app.js :heavy_check_mark:<br>
+   - sass lint fix :heavy_check_mark:<br>
+
+6. Styling nav and hero section :heavy_check_mark:<br>
+7. Responsive hero section :heavy_check_mark:<br>
+8. Add SVG to FONT SVG converter task :heavy_check_mark:<br>
+9. Styling about section :heavy_check_mark:<br>
+10. multiselect **header, **subheader, **title, **subtitle :heavy_check_mark:<br>
+11. Include google map (map section) :heavy_check_mark:<br>
+12. Add contact section
+
+    - install validator.js :heavy_check_mark:<br>
+    - responsive form :heavy_check_mark:<br>
+
+13. Add footer :heavy_check_mark:<br>
+14. Add router :heavy_check_mark:<br>
+15. Add controller :heavy_check_mark:<br>
